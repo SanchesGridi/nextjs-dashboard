557b8258e3a3bede41eb45a87d9dbc48bc3c46cc
Alexander Gridin <sanchesgridi@gmail.com>
2024-03-20 19:06:02 +0300

    chapter 9 (slow the fetchRevenue())

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -19,12 +19,12 @@ export async function fetchRevenue() {
         // Artificially delay a response for demo purposes.
         // Don't do this in production :)
 
-        // console.log('Fetching revenue data...');
-        // await new Promise((resolve) => setTimeout(resolve, 3000));
+        console.log('Fetching revenue data...'); // todo: comment that after chapter 9
+        await new Promise((resolve) => setTimeout(resolve, 3000)); // todo: comment that after chapter 9
 
         const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-        // console.log('Data fetch completed after 3 seconds.');
+        console.log('Data fetch completed after 3 seconds.'); // todo: comment that after chapter 9
 
         return data.rows;
     } catch (error) {
