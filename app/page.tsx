bf3a461499e9ac70141451dc0379f0de1255c337
SanchesGridi <sanchesgridi@gmail.com>
2024-03-25 01:17:38 +0300

    test online commit (https://stackblitz.com/)

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import Image from 'next/image';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 
+// test
+
 // route: "/"
 export default function Page() {
     return (
