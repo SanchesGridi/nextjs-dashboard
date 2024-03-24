4a4eb8befe1db02aa3d818ddcb5f25bbd574f69a
Alexander Gridin <sanchesgridi@gmail.com>
2024-03-24 17:26:30 +0300

    [CustomersTable] little change

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -3,11 +3,7 @@ import { lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
 import { CustomersTableType, FormattedCustomersTable } from '@/app/lib/definitions';
 
-export default async function CustomersTable({
-    customers,
-}: {
-    customers: FormattedCustomersTable[];
-}) {
+export default async function CustomersTable({ customers }: { customers: FormattedCustomersTable[]; }) {
     return (
         <div className="w-full">
             <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
